781bb065ef7f5328e036cc58a6e02f7b3fa0aa2f
sipasi <tanksasha.work@gmail.com>
2023-03-12 23:43:51 +0200

    Have added a script that limits the frame rate

---
+++ Assets/Code/Shared/Core/FrameRateLimit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Framework.Shared.Core
+{
+    public class FrameRateLimit : MonoBehaviour
+    {
+        [SerializeField] private Preset preset;
+
+        void Awake()
+        {
+            Application.targetFrameRate = (int)preset;
+        }
+
+        private void OnValidate()
+        {
+            Application.targetFrameRate = (int)preset;
+        }
+
+        private enum Preset
+        {
+            None,
+            Set30 = 30,
+            Set60 = 60,
+            Set120 = 120,
+            Set240 = 240,
+        }
+    }
+}
