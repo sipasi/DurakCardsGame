c4c3de947d49af5fa57e17498cdd644aaba7e9ef
sipasi <tanksasha.work@gmail.com>
2023-03-18 21:58:29 +0200

    Remove unused attributes and folder

--- Assets/Code/Shared/DependencyInjection/Attributes/InjectAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-
-namespace Framework.Shared.DependencyInjection
-{
-    [Obsolete("Not implement")]
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
-    public class InjectAttribute : Attribute { }
-}
