8438170ff0fefeefbd7736aa1845dba9ac2ecbba
sipasi <tanksasha.work@gmail.com>
2023-03-18 21:57:20 +0200

    DiContainerBuilder finds a MonoBehaviour objects by IConfigurator interface

--- Assets/Code/Durak/DependencyInjection/DiContainerBuilder.cs
+++ Assets/Code/Durak/DependencyInjection/DiContainerBuilder.cs
@@ -1,8 +1,8 @@
 ﻿
 using System;
+using System.Linq;
 
 using Framework.Shared.DependencyInjection;
-using Framework.Shared.DependencyInjection.Unity;
 
 using UnityEngine;
 
@@ -15,7 +15,7 @@ namespace Framework.Durak.DependencyInjection
         {
             var builder = new ServiceBuilder();
 
-            IConfigurator[] configurators = FindObjectsOfType<ServiceConfigurator>();
+            var configurators = FindObjectsOfType<MonoBehaviour>().OfType<IConfigurator>();
 
             foreach (var configurator in configurators)
             {
